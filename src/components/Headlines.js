4506fa11cabdd9d1abb94bd64c075258e006a36f
Kate Kiatsiri <keidsiri@gmail.com>
2022-02-28 11:11:24 -0800

    add Headlines.js

---
+++ src/components/Headlines.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Headlines extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      headlines: []
+    };
+  }
+
+  render() {
+  }
+}
+
+export default Headlines;
